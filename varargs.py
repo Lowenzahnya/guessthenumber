02e4ed65329be880d0e1953119b152d9183b955d
Lowenzahnya <lowenzahnya@yandex.ru>
2023-04-30 11:48:24 +0300

    Pomogite

---
+++ varargs.py
@@ -0,0 +1,11 @@
+def total(a=5, *numbers, **phonebook):
+    print('a', a)
+    # проход по всем элементам кортежа
+    for single_item in numbers:
+        print('single_item', single_item)
+    # проход по всем элементам словаря
+    for first_part, second_part in phonebook.items():
+        print(first_part, second_part)
+
+
+print(total(4, 6, 7, 8, Jack=1123, John=2231, Inge=1560))
